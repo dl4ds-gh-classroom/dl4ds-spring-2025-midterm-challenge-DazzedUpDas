404d065da628fa9699856c19fde5b74b11344808
DazzedUpDas <161445393+DazzedUpDas@users.noreply.github.com>
2025-03-27 22:31:24 -0400

    Checked SimpleCNN.py

--- SimpleCNN.py
+++ SimpleCNN.py
@@ -151,6 +151,7 @@ def main():
         "model": "SimpleCNN_1",   # Change name when using a different model
         "batch_size": 128, # run batch size finder to find optimal batch size
         "learning_rate": 1e-3,
+        "epochs":5,
         "num_workers": 2, # Adjust based on your system
         "device": "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu",
         "data_dir": "./data",  # Make sure this directory exists
